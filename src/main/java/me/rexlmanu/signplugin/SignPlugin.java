4afe0adadc767e7e9f20b403ba3ac8a77b337ac6
rexlManu <rexlmanude@gmail.com>
2019-04-28 00:27:04 +0200

    Initial Commit

---
+++ src/main/java/me/rexlmanu/signplugin/SignPlugin.java
@@ -0,0 +1,114 @@
+/*
+ * © Copyright - Emmanuel Lampe aka. rexlManu 2019.
+ */
+package me.rexlmanu.signplugin;
+
+import com.google.common.collect.Lists;
+import me.rexlmanu.signplugin.utility.TagUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/******************************************************************************************
+ *    Urheberrechtshinweis                                                                *
+ *    Copyright © Emmanuel Lampe 2019                                                  *
+ *    Erstellt: 27.04.2019 / 22:55                                               *
+ *                                                                                        *
+ *    Alle Inhalte dieses Quelltextes sind urheberrechtlich geschützt.                    *
+ *    Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet,            *
+ *    bei Emmanuel Lampe. Alle Rechte vorbehalten.                                        *
+ *                                                                                        * 
+ *    Jede Art der Vervielfältigung, Verbreitung, Vermietung, Verleihung,                 *
+ *    öffentlichen Zugänglichmachung oder andere Nutzung                                  *
+ *    bedarf der ausdrücklichen, schriftlichen Zustimmung von Emmanuel Lampe.             *
+ ******************************************************************************************/
+
+public final class SignPlugin extends JavaPlugin {
+
+    private SimpleDateFormat dateFormat, timeFormat;
+
+    @Override
+    public void onEnable() {
+        this.getConfig().options().copyDefaults(true);
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        this.dateFormat = new SimpleDateFormat(this.getMessage("DateFormat"));
+        this.timeFormat = new SimpleDateFormat(this.getMessage("TimeFormat"));
+    }
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+
+        if (! (sender instanceof Player)) {
+            sender.sendMessage(this.getMessage("ConsoleSender"));
+            return true;
+        }
+        final Player player = (Player) sender;
+        final ItemStack itemInHand = player.getItemInHand();
+        if (itemInHand == null) {
+            sender.sendMessage(this.getMessage("NoItemInHand"));
+            return true;
+        }
+
+        final int heldItemSlot = player.getInventory().getHeldItemSlot();
+        final boolean signed = TagUtils.getBooleanMeta(player, heldItemSlot, "Signed");
+
+        switch (label.toLowerCase()) {
+            case "sign":
+                if (signed && ! this.signOverride()) {
+                    player.sendMessage(this.getMessage("ItemAlreadySigned"));
+                    return true;
+                }
+                player.sendMessage(this.getMessage(signed ? "SignOverride" : "SignSuccess"));
+                this.signItem(player, itemInHand);
+                TagUtils.setBooleanMeta(player, heldItemSlot, "Signed", true);
+                return true;
+            case "unsign":
+                if (! signed) {
+                    player.sendMessage(this.getMessage("NotSigned"));
+                    return true;
+                }
+                final ItemMeta itemMeta = itemInHand.getItemMeta();
+                itemMeta.setLore(Lists.newArrayList());
+                itemInHand.setItemMeta(itemMeta);
+                player.sendMessage(this.getMessage("UnsignSuccess"));
+                TagUtils.setBooleanMeta(player, heldItemSlot, "Signed", false);
+                return true;
+        }
+        return false;
+    }
+
+    private void signItem(final Player player, final ItemStack itemInHand) {
+        final ItemMeta itemMeta = itemInHand.getItemMeta();
+        final List<String> lore = Lists.newArrayList();
+        final Date date = new Date();
+        this.getConfig().getStringList("Messages.SignFormat").forEach(s -> {
+            lore.add(s
+                    .replace("%NAME%", player.getName())
+                    .replace("%DATUM%", this.dateFormat.format(date))
+                    .replace("%UHRZEIT%", this.timeFormat.format(date))
+                    .replace('&', '§')
+            );
+        });
+        itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        itemMeta.setLore(lore);
+        itemInHand.setItemMeta(itemMeta);
+    }
+
+    private String getMessage(final String key) {
+        return ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("Messages." + key));
+    }
+
+    private boolean signOverride() {
+        return this.getConfig().getBoolean("Settings.ResignAllowed");
+    }
+}
---
+++ src/main/java/me/rexlmanu/signplugin/utility/TagUtils.java
@@ -0,0 +1,83 @@
+/*
+ * © Copyright - Emmanuel Lampe aka. rexlManu 2019.
+ */
+package me.rexlmanu.signplugin.utility;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+/******************************************************************************************
+ *    Urheberrechtshinweis                                                                *
+ *    Copyright © Emmanuel Lampe 2019                                                  *
+ *    Erstellt: 27.04.2019 / 23:13                                               *
+ *                                                                                        *
+ *    Alle Inhalte dieses Quelltextes sind urheberrechtlich geschützt.                    *
+ *    Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet,            *
+ *    bei Emmanuel Lampe. Alle Rechte vorbehalten.                                        *
+ *                                                                                        * 
+ *    Jede Art der Vervielfältigung, Verbreitung, Vermietung, Verleihung,                 *
+ *    öffentlichen Zugänglichmachung oder andere Nutzung                                  *
+ *    bedarf der ausdrücklichen, schriftlichen Zustimmung von Emmanuel Lampe.             *
+ ******************************************************************************************/
+
+public final class TagUtils {
+
+    public static void setBooleanMeta(final Player player, final int slot, final String key, final boolean value) {
+        try {
+            final Object itemFromSlot = getItemFromSlot(player, slot);
+            Object tag = getTagFromItem(itemFromSlot);
+            if (Objects.isNull(tag)) {
+                final Object nbtTagCompound = getNMSClass("NBTTagCompound").getConstructor().newInstance();
+                Objects.requireNonNull(itemFromSlot).getClass().getMethod("setTag", nbtTagCompound.getClass())
+                        .invoke(itemFromSlot, nbtTagCompound);
+                tag = getTagFromItem(itemFromSlot);
+            }
+            final Method method = Objects.requireNonNull(tag).getClass().getMethod("setBoolean", String.class, boolean.class);
+            method.invoke(tag, key, value);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean getBooleanMeta(final Player player, final int slot, final String key) {
+        try {
+            final Object tag = getTagFromItem(getItemFromSlot(player, slot));
+            final Method method = Objects.requireNonNull(tag).getClass().getMethod("getBoolean", String.class);
+            return (boolean) method.invoke(tag, key);
+        } catch (final Exception e) {
+            return false;
+        }
+    }
+
+    private static Object getTagFromItem(final Object item) {
+        try {
+            return Objects.requireNonNull(item).getClass().getMethod("getTag").invoke(item);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Object getItemFromSlot(final Player player, final int slot) {
+        try {
+            final Method getHandle = player.getClass().getMethod("getHandle");
+            final Object nmsPlayer = getHandle.invoke(player);
+            final Object inventory = nmsPlayer.getClass().getField("inventory").get(nmsPlayer);
+            return inventory.getClass().getMethod("getItem", int.class).invoke(inventory, slot);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Class<?> getNMSClass(final String nmsClassString) throws ClassNotFoundException {
+        final String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
+        final String name = "net.minecraft.server." + version + nmsClassString;
+        return Class.forName(name);
+    }
+
+
+}
